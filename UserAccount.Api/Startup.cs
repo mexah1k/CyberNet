b15d7da91419c96dfbe3a46f0f25f432bf370c7b
Mykhailo.Polishscuk <mykhailo.polishchuk@keros-digital.com>
2017-11-24 18:38:04 +0200

    Logger interface was added tot startup.

--- UserAccount.Api/Startup.cs
+++ UserAccount.Api/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace UserAccount.Api
 {
@@ -26,7 +27,7 @@ namespace UserAccount.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory factory)
         {
             if (env.IsDevelopment())
             {
