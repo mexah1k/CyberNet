57f44174f07c09eb2237ed670390bc574494e923
Mykhailo.Polishscuk <mykhailo.polishchuk@keros-digital.com>
2018-04-02 17:19:14 +0300

    PagedList was added.

---
+++ Core/Infrastructure/Pagination/PagedList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Infrastructure.Pagination
+{
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; }
+
+        public int TotalPages { get; }
+
+        public int PageSize { get; }
+
+        public int TotalCount { get; }
+
+        public bool HasPrevios => CurrentPage > 1;
+
+        public PagedList(List<T> listItems, int pageNumber, int pageSize, int count)
+        {
+            CurrentPage = pageNumber;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            PageSize = pageSize;
+            TotalCount = count;
+
+            AddRange(listItems);
+        }
+
+        public static PagedList<T> CreatePagedList(IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var items = source
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return new PagedList<T>(items, pageNumber, pageSize, source.Count());
+        }
+    }
+}
